1e52df587a31862bf3d1fe88531f42368cd64d6d
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2021-11-05 16:40:01 -0300

    feat: machine running!

--- turingMachine.js
+++ turingMachine.js
@@ -1,17 +1,3 @@
-/**
- *  Uma maquina de Turing é definida como uma sétupla
- *  M = {
- *    Q = estados (q0, q1, q2...),
- *    Σ = alfabeto (todas as letras da maquina),
- *    r = sequencia a ser verificada,
- *    q0 = estado inicial,
- *    F = estado final,
- *    ᗣ = caracter vazio,
- *    ઠ = objeto com todos os comandos, no formato (q0,1,q1,A,R)
- * 
- *    Por motivos de incompatibilidade, os icones foram substituidos por texto
- *  }
- */
 
 //*************** FUNCTIONS ********************* */
 function stringToArray(str){
@@ -28,14 +14,14 @@ function girininho(str){
   const array = stringToArray(str)
   return {
     read: {
-      currentState = array[0],
-      char = array[1],
+      currentState: array[0],
+      char: array[1],
     },
-      execute: {
-        goState = array[2],
-        write = array[3],
-        direction = array[4]
-      }
+    execute: {
+      goState: array[2],
+      write: array[3],
+      direction: array[4].toUpperCase()
+    }
   }
 }
 
@@ -45,10 +31,11 @@ function TM(obj){
   obj.test= stringToArray(obj.test)
   
   let fita = []
-  for(let i=0; i<4; i++) fita.push(obj.empty) //4 caracteres vazios no inicio
-  fita = [...fita, obj.test]                  //caracteres de teste no meio
-  for(let i=0; i<4; i++) fita.push(obj.empty) //4 caracteres vazios no final
-                                              //§§§§123123§§§§
+  for(let i=0; i<4; i++) fita.push(obj.empty) // 4 caracteres vazios no inicio
+  fita = fita.concat(obj.test)                // caracteres de teste no meio
+  for(let i=0; i<4; i++) fita.push(obj.empty) // 4 caracteres vazios no final
+                                              // §,§,§,§,1,2,3,1,2,3,§,§,§,§
+
   return {...obj, fita}
 }
 
@@ -61,45 +48,59 @@ function directionToInt(str){
 }
 
 function calculate(tm){
-  let ponteiroFita= 4
-  let currentStateFita= tm.initState
-  let fitaLength= tm.fita.length()
+  const error = "\n\nNão foi possível resolver essa máquina de Turing"
+  let ponteiroFita = 4
+  let currentStateFita = tm.initState
   let direction
 
   for(;;){ //percorre a fita e salva o caractere apontado 
     let currentCharacterFita = tm.fita[ponteiroFita]
+    let count = 0
 
-    for(let girininho in tm.commands){ //percorre todos os girininhos, em busca do "read match" 
+    for(let girininho of tm.commands){ //percorre todos os girininhos, em busca do "read match" 
       if(girininho.read.currentState == currentStateFita && girininho.read.char == currentCharacterFita){ //se encontrar, executa a acao do girininho
         currentStateFita = girininho.execute.goState            //vai para tal estado
         tm.fita[ponteiroFita] = girininho.execute.write         //substitui o caractere
         direction = directionToInt(girininho.execute.direction) //vai para tal direcao
+        ponteiroFita = ponteiroFita + direction                 //update no ponteiro
+        
+        // Se detectar direction "S" e o estado final informado,
+        // o algoritmo termina e retorna a fita
+        if(direction == 0 && currentStateFita == tm.endState){
+          let result = tm.fita.toString()
+          result = result.replace(/,/g," ")
+          result = "\nResult: " + result
+          return result
+        }
+        
+        // Caso contrário, apenas continue
+        break
       }
+      
+      count++ //contador para verificar se todos os comandos foram visitados
+      if(count == tm.commands.length) return error
     }
-
   }
 }
 
 //********************** MAQUINA DE TURING ************************ */
 const turingMachine = TM({
   states: "0,1",
-  alphabet: "a,b,c,1,2,3,§",
-  test: "a,b,c,a,b,c",
+  alphabet: "a,1,¨", 
+  test: "abcabc",
   initState: "0",
   endState: "1",
-  empty: "§",
+  empty: "¨",
   commands: [
     girininho("0,a,0,1,R"),
     girininho("0,b,0,2,R"),
     girininho("0,c,0,3,R"),
-    girininho("0,§,1,§,S")
+    girininho("0,¨,1,¨,S")
   ]
 })
 
-calculate(turingMachine)
+const result= calculate(turingMachine)
+console.log(result)
 
 //********************END MAQUINA DE TURING************************ */
-
-
-
 
