8b5c2994b8ff83ee4f559c07439021e8ed47075a
Rafael Bortolozo <62819159+RafaelBortolozo@users.noreply.github.com>
2021-12-03 17:17:21 -0300

    feat: init form

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# TuringMachine
+Máquina de Turing, script para a disciplina de Teoria da Computação.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
 
-function App() {
+const App = () => {
+  const title = 'Máquina de Turing'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title}</h1>
+        <form>
+          <TextField 
+            required 
+            id="states" 
+            placeholder="0,1,2,3,4..." 
+            label="Q (states)" />
+
+          <TextField 
+            required 
+            id="alphabet" 
+            placeholder="a,b,c,1,2,3,▢" 
+            label="Σ (alphabet)" />
+
+          <TextField 
+            required 
+            id="test" 
+            placeholder="abcabc" 
+            label="୮ (word)" />
+
+          <TextField 
+            required 
+            id="empty" 
+            placeholder="▢" 
+            label="▢ (Empty)" />
+
+          <TextField 
+            required 
+            id="initState" 
+            placeholder="0" 
+            label="q0 (initState)" />
+          
+          <TextField 
+            required 
+            id="endState" 
+            placeholder="2,4" 
+            label="F (endStates)" />
+          
+          <TextField 
+            required 
+            id="commands" 
+            placeholder="0,a,1,a,R" 
+            label="ઠ (commands)" />
+          
+          <Button variant="contained" color="primary">
+            +
+          </Button>
+          <Button variant="contained" color="primary">
+            Calcular
+          </Button>
+        </form>
     </div>
-  );
+  )
 }
 
 export default App;
